8e2fa7a0fa9a785e6e6943b9e08cdc8db01935e0
Stephen Habegger <stephen.p.habegger@gmail.com>
2017-11-03 12:25:30 -0500

    Correctly factoring

--- src/util/factorize.js
+++ src/util/factorize.js
@@ -1,112 +1,7 @@
 'use strict';
 
-const primes = new Set([ 2, 3, 5, 7, 11, 13, 17, 19 ]);
-
-// How many times does 2 evenly divide n
-function exp2(n) {
-  let count = 0;
-
-  while ((n & 1) === 0) {
-    n = n >> 1;
-    count++;
-  }
-
-  return count;
-}
-
-function primeWitness(n, a, d, r) {
-  const nMinusOne = n - 1;
-
-  let x = (a ** d) % n;
-
-  if (x === 1 || x === nMinusOne) return true;
-
-  while (--r) {
-    x = (x * x) % n;
-    if (x === 1) return false;
-    if (x === nMinusOne) return true;
-  }
-}
-
-// An implementation of Miller-Rabin primality test
-function isPrime(n) {
-  if (primes.has(n)) return true;
-
-  const nMinusOne = n - 1;
-  const s = exp2(nMinusOne);
-  const d = nMinusOne / (1 << s);
-  const lnN = Math.log(n);
-  const maxA = Math.min(nMinusOne, Math.floor(2 * lnN * lnN));
-
-  let a;
-  for (a = 2; a <= maxA; a++) {
-    if (!primeWitness(n, a, d, s)) return false;
-  }
-
-  primes.add(n);
-  return true;
-}
-
-// Greatest Common Denominator
-function gcd(a, b) {
-  let remainder;
-
-  while (b != 0) {
-    remainder = a % b;
-    a = b;
-    b = remainder;
-  }
-
-  return a;
-}
-
-// Polynomial mod n for Pollard
-function g(x, n) {
-  return (x * x + 1) % n;
-}
-
-// An implementation of Pollard's Rho Algorithm for integer factorization
-function getFactor(n, s = 2) {
-  if (isPrime(n)) return n;
-
-  const sqrt = Math.sqrt(n);
-  if (Number.isInteger(sqrt)) return sqrt;
-
-  let x = s,
-      y = s,
-      d = 1;
-
-  while (d === 1) {
-    x = g(x, n);
-    y = g(g(y, n), n);
-    d = gcd(Math.abs(y - x), n);
-  }
-
-  if (d === n) {
-    return getFactor(n, s + 1);
-  }
-
-  return d;
-}
-
-function checkFactor(f, factors) {
-  if (f === 1) return;
-
-  if (isPrime(f)) {
-    if (!factors.has(f)) factors.set(f, 1);
-    else factors.set(f, factors.get(f) + 1);
-  } else {
-    factorizeEngine(f, factors);
-  }
-}
-
-function factorizeEngine(n, factors) {
-  const a = getFactor(n);
-  const b = n / a;
-
-  checkFactor(a, factors);
-  checkFactor(b, factors);
-}
+const primes = new Set([ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43 ]);
+const sequentialPrimes = new Set(primes);
 
 export default function factorize(number) {
   if (!Number.isInteger(number)) {
@@ -117,22 +12,60 @@ export default function factorize(number) {
     throw new Error('Input must be positive');
   }
 
-  if (number === 1) return [];
-
-  const factors = new Map();
-
-  const twoCount = exp2(number);
-  if (twoCount) {
-    factors.set(2, twoCount);
+  if (primes.has(number)) {
+    return [{
+      prime: number,
+      multiplicity: 1
+    }];
   }
 
-  factorizeEngine(number >> twoCount, factors);
-
   const result = [];
-  factors.forEach((val, key) => result.push({
-    prime: key,
-    multiplicity: val
-  }));
 
-  return result.sort((a, b) => a.prime - b.prime);
+  const maxFactor = Math.floor(Math.sqrt(number));
+  const primeIter = sequentialPrimes.values();
+
+  const checkFactor = () => {
+    const factor = {
+      prime: nextPrime,
+      multiplicity: 0
+    };
+
+    while (number % nextPrime === 0) {
+      number /= nextPrime;
+      factor.multiplicity++;
+    }
+
+    return factor;
+  };
+
+  let nextIter = primeIter.next();
+  let nextPrime = nextIter.value;
+  while (!nextIter.done && nextPrime <= maxFactor) {
+    const factor = checkFactor();
+    if (factor.multiplicity) result.push(factor);
+
+    nextIter = primeIter.next();
+    if (!nextIter.done) nextPrime = nextIter.value;
+  }
+
+  nextPrime++;
+  while (nextPrime <= maxFactor) {
+    const factor = checkFactor();
+    if (factor.multiplicity) {
+      primes.add(nextPrime);
+      result.push(factor);
+    }
+
+    nextPrime++;
+  }
+
+  if (number !== 1) {
+    primes.add(number);
+    result.push({
+      prime: number,
+      multiplicity: 1
+    });
+  }
+
+  return result;
 }
